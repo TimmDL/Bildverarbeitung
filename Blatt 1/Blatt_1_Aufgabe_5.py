03297242cb65eafaa1c86715216811408eb73b74
Fabidihh <58420848+Fabidihh@users.noreply.github.com>
2020-05-05 15:45:36 +0200

    Update Blatt_1_Aufgabe_5.py

--- Blatt 1/Blatt_1_Aufgabe_5.py
+++ Blatt 1/Blatt_1_Aufgabe_5.py
@@ -1,48 +1,87 @@
 # Blatt 1 - Aufgabe 5
 
+
+
 import numpy as np
+
 from skimage.io import imread, imsave
+
 import time
 
+
+
 # 1)
+
 def testePixelLoop (img):
+
     for n in range(img.shape[0]):
+
         for m in range(img.shape[1]):
+
             if 99 < img[n,m] < 200:
+
                 pass # do nothing
+
           
 
+
+
 # 2)
- #  def testePixelBroadcast (img):
-       
+
+def testePixelBroadcast (img):
+ for imgsum in np.nditer(img):
+        if 99 < imgsum < 200:
+            pass
+
+
 
 # 3)
+
 img = imread("mandrill.png")
+
     
+
 # 100 Aufrufe der Schleifen Funktion
+
 tic = time.time()
+
 i = 0
+
 while i < 100:
+
     testePixelLoop(img) 
+
     i += 1
+
 toc = time.time()
+
 diffLoop = toc - tic
+
 print("Rechendauer mit Schleife: " +  str(diffLoop))  
+
     
+
 # 100 Aufrufe der Broadcast Funktion
+
 tic = time.time()
+
 i = 0
-   ## while i < 100:
-        # testePixelBroadcast(img) 
-       ##   i += 1
+
+while i < 100:
+
+    testePixelBroadcast(img) 
+
+    i += 1
+
 toc = time.time()
+
 diffBroad = toc - tic
+
 print("Rechendauer mit Broadcasting: " +  str(diffBroad))
+
+
+
 
 
 print("Differenz: " + str(diffLoop - diffBroad))
 
-
-       
-    
-    
