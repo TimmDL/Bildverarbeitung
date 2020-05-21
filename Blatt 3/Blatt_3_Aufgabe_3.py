1610f9e633afca9adb6151e0d4e62f59e7a1d1a3
Jan Harder <63193780+Harderhh@users.noreply.github.com>
2020-05-21 12:23:40 +0200

    333

--- Blatt 3/Blatt_3_Aufgabe_3.py
+++ Blatt 3/Blatt_3_Aufgabe_3.py
@@ -6,3 +6,62 @@ from skimage.io import imread
 
 import matplotlib.pyplot as plt
 
+fehler = imread("mitFehler.png")
+original = imread("ohneFehler.png")
+
+# Aufgabenteil 1
+dif =  original - fehler 
+add = dif + original
+
+plt.imshow(add, cmap="gray")
+
+#Aufgabenteil 2
+dif[dif>1] = 1
+plt.imshow(dif, cmap="gray")
+
+#Aufgabenteil 3
+# a)
+coor = np.nonzero(dif)
+
+
+coor = np.asarray(coor)
+print(coor)
+
+
+
+
+def count(arr):
+     fehler = 0
+     comp_arr = np.full(arr.shape, False, dtype=bool) #initialize
+     arr_height = arr.shape[0]
+     arr_width = arr.shape[1]
+
+     for i in range(arr_height): #Row
+        for j in range(arr_width): #column
+
+            center = arr[i,j]
+
+            #Check edges
+            if i == 0: #left side
+                left = arr[i,j]
+            else:
+                left = arr[i-1], j]
+
+            if i == arr_height - 1: #right side
+                right = arr[i,j]
+            else:
+                right = arr[i+1,j]
+
+            if j == 0: #up
+                up = arr[i,j]
+            else:
+                up = arr[i, j-1]
+
+            if j == arr_width - 1: #down
+                down = arr[i,j]
+            else:
+                down = arr[i, j+1]
+                
+            print(left) 
+            
+count(coor)
