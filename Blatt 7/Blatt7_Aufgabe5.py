861b1ded164c17b9ab01418fd7a976e2d90c2890
Jan Harder <63193780+Harderhh@users.noreply.github.com>
2020-06-25 14:34:52 +0200

    Aufgabenteil2

--- Blatt 7/Blatt7_Aufgabe5.py
+++ Blatt 7/Blatt7_Aufgabe5.py
@@ -5,6 +5,7 @@ import numpy as np
 from skimage.io import imread, imsave, imshow
 
 import skimage.util as util
+import skimage.filters as filters
 
 import matplotlib.pyplot as plt
 
@@ -28,3 +29,34 @@ plt.imshow(gausEinstein, cmap="gray")
 # Der Kragen, die Auggen und der Schnurrbart lassen sich noch relativ gut erkennen.
 # Die Gesichtszüge nicht mehr so gut
 #
+
+# Aufgabenteil 2
+ballon = imread("ballon.png")
+plt.imshow(ballon)
+
+
+# Das Bild zeigt einen Ballon. In der mitte des Bildes ist ein gelber Kreis.
+# Um den mittleren Kreis bilden sich mehrere Ringe, wobei sich die verschiedenen 
+# Farben spiral-förmig nach außen drehen
+
+sigma2 = 10
+
+def gausFarb(img, sig):
+    result = filters.gaussian(img,sigma=sig,multichannel=True)
+    return result
+
+gausBallon = gaus(ballon,sigma2)
+
+plt.imshow(gausBallon, cmap="gray")
+
+# Bei sigma = 10 lässt sich die spiralförmige Anordnung der Farben noch erkennen
+# Bei sigma = 20 lässt sie sich nur noch erahnen.
+# In beiden Fällen lässt sich aber nichts anderes erkennen, außer dass
+# im Bild viele verschiedene Farben verwendet werden.
+# Umso höher sigma wird, desto weniger gemischte Farben gibt es (sonst nur Rot,gelb,blau)
+
+
+
+
+
+
